36899e797e19166e64505b77a5a2740ce6f87885
MediaMagnet <nathanec@gmail.com>
2018-07-23 19:51:01 -0500

    updated light-twitch

--- light-twitch.rb
+++ light-twitch.rb
@@ -2,15 +2,29 @@
 require 'eventmachine'
 require 'websocket-eventmachine-client'
 require 'huey'
-
-
+require 'configatron'
+require_relative 'config.rb'
+=begin
 Huey.configure do |config|
-  config.hue_ip = '192.168.1.244'
-  config.uuid = "NqysWo8Fmlm4WE8M6Sxm-9UznfDoSnmFJOmg7q2g"
+ config.hue_ip = '192.168.1.244'
+ config.uuid = "NqysWo8Fmlm4WE8M6Sxm-9UznfDoSnmFJOmg7q2g"
 end
+=end
 
+#bulb = Huey::Bulb.find("#{rand(1..4)}")
 
-#bulb = Huey::Bulb.find('Desk')
+$msg = nil
+
+def partymode()
+	lightmsg = $msg.split(';').select{ |word|
+		word.include?('color')
+	}.to_s
+
+	puts lightmsg
+	lightmsg1 = lightmsg.split('=').at(-1).delete('\"]')
+	puts lightmsg1
+	#bulb.update(rgb: lightmsg1)
+end
 
 EM.run do
 	ws = WebSocket::EventMachine::Client.connect(:host => 'irc-ws.chat.twitch.tv', :port => 80, :ssl => false)
@@ -24,47 +38,54 @@ EM.run do
 	end
 
 	ws.onmessage do |msg, type|
-    if msg.include?('PING') == true
-      puts "Received message: #{msg.strip}"
-      ws.send "PONG :tmi.twitch.tv"
-      ws.pong
-    elsif msg.include?('PRIVMSG')
-				lightmsg = msg.split(';').at(1)
-        puts lightmsg
-        lightmsg1 = lightmsg.split('=').at(-1)
-				puts lightmsg1
-				Huey::Bulb.all.update(rgb: lightmsg1)
-        #bulb.update(rgb: lightmsg1)
-    else
-      puts "Received message: #{msg.strip}"
+		if msg.include?('PING') == true
+			puts "Received message: #{msg.strip}"
 
-    end
+			ws.send "PONG :tmi.twitch.tv"
+			ws.pong
+		elsif msg.include?('PRIVMSG')
+			if msg.include?('bits=500')
 
-	end
+				#make this loop for a minute.
+=begin
+ loop do
+ $msg = msg
+ partymode
+=end
 
-	ws.onclose do |code, reason|
-		puts "Disconnected with status code: #{code} #{reason}"
+ end
 
-	end
+ else
+ puts "Received message: #{msg.strip}"
 
-	ws.onerror do |error|
-		puts "Error: #{error}"
+ end
 
-	end
+ end
 
-	ws.onping do |message|
-		puts "Ping received: #{message}"
-    ws.pong "PONG :tmi.twitch.tv"
-  end
+ ws.onclose do |code, reason|
+ puts "Disconnected with status code: #{code} #{reason}"
 
-  ws.onpong do |message|
-    puts "Pong sent: #{message}"
-  end
+ end
+
+ ws.onerror do |error|
+ puts "Error: #{error}"
+
+ end
+
+ ws.onping do |message|
+ puts "Ping received: #{message}"
+
+ ws.pong "PONG :tmi.twitch.tv"
+ end
+
+ ws.onpong do |message|
+ puts "Pong sent: #{message}"
+
+ end
 
 
-
-	EventMachine.next_tick do
-		puts "tick!"
-	end
-end
+ EventMachine.next_tick do
+ puts "tick!"
+ end
+ end
 
