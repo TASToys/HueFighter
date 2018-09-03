5c5710eb606d2acbec6ff7db7c242dc1a668b6d3
MediaMagnet <nathanec@gmail.com>
2018-09-03 16:45:07 -0500

    I did a thing not sure what I did though...

--- HueFighter.rb
+++ HueFighter.rb
@@ -44,12 +44,11 @@ def partymode()
 	loop do
 		break if @i == 240 # Let's leave our loop
 		color = SecureRandom.hex(3)
-		puts "Set color to ##{color} on light #{rand(1..4)}"
+		puts "Set color to ##{color} on light #{rand(1..4)} at #{@i}"
 		Huey::Bulb.all.update(rgb: "##{color}")
 
 		@i += 1
 		sleep 0.25
-
 	end
 end
 
