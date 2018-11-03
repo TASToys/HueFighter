ab4d53d350ce3ff1559b531916bd4f16c577eded
MediaMagnet <nathanec@gmail.com>
2018-11-03 17:30:32 -0500

    HueFighter and bridge script cleaned up a bit more to rubocop.

--- bridge-connect.rb
+++ bridge-connect.rb
@@ -1,28 +1,33 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'configatron'
 require 'rest-client'
 require 'tty-prompt'
 require 'json'
-require_relative 'config.rb'
+require_relative 'config/config.rb'
+
+# rubocop:disable Metrics/LineLength, Style/BracesAroundHashParameters
 
 prompt = TTY::Prompt.new
 
-name = prompt.ask("What would you like the app to be called on your Hue bridge?") do |q|
-	q.required true
-	q.default 'HueFighter'
+name = prompt.ask('What would you like the app to be called on your Hue bridge?') do |q|
+  q.required true
+  q.default 'HueFighter'
 end
 
-prompt.keypress("To use HueFighter you will need to pair with your Hue Bridge. Press the button on your bridge and then press enter when ready.", keys: [:return], timeout: 30)
+prompt.keypress('To use HueFighter you will need to pair with your Hue Bridge. Press the button on your bridge and then press enter when ready.', keys: [:return], timeout: 30)
 
-
-response = RestClient.post "http://#{configatron.bridge}/api", {"devicetype":"#{name}" }.to_json, { content_type: :json, accept: :json }
+response = RestClient.post "http://#{configatron.bridge}/api", { "devicetype": name.to_s }.to_json, { content_type: :json, accept: :json }
 
 body = JSON.parse(response)[0]
 
 if body.fetch('error').fetch('description')
-	puts body.fetch('error').fetch('description')
+  puts body.fetch('error').fetch('description')
 elsif body.fetch('success').fetch('username')
-	puts "Please add this username to your config.rb file"
-	puts body.fetch('success').fetch('username')
+  puts 'Please add this username to your config.rb file'
+  puts body.fetch('success').fetch('username')
 
 end
+
+# rubocop:enable Metrics/LineLength, Style/BracesAroundHashParameters
